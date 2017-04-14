eedea421faad8f8bf6051235569b5b321b5467a4
maartenplieger <maartenplieger@gmail.com>
2017-04-14 14:18:35 +0200

    Springboot war and jar packaging into single war file

--- src/main/java/nl/knmi/adaguc/AdagucServicesApplication.java
+++ src/main/java/nl/knmi/adaguc/AdagucServicesApplication.java
@@ -1,19 +1,35 @@
 package nl.knmi.adaguc;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AdagucServicesApplication {
+public class AdagucServicesApplication extends SpringBootServletInitializer{
 
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application)  {
 		return application.sources(AdagucServicesApplication.class);
 	}
 
+	
 	public static void main(String[] args) {
-		SpringApplication.run(AdagucServicesApplication.class, args);
-	}
+        configureApplication(new SpringApplicationBuilder()).run(args);
+    }
 
+    private static SpringApplicationBuilder configureApplication(SpringApplicationBuilder builder) {
+        return builder.sources(AdagucServicesApplication.class).bannerMode(Banner.Mode.OFF);
+    }
+//	public static void main(String[] args) {
+//		SpringApplication.run(AdagucServicesApplication.class, args);
+//	}
+//	
 
+//    private static SpringApplicationBuilder configureApplication(SpringApplicationBuilder builder) {
+//        return builder.sources(AdagucServicesApplication.class).bannerMode(Banner.Mode.OFF);
+//    }
+
+	
 }
