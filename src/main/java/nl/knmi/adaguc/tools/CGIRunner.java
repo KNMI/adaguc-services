a59429bb5483737cb018d6a05bdc3da1471bfa43
Maarten Plieger <maartenplieger@gmail.com>
2020-05-06 14:30:15 +0200

    Adaguc-services now writes an info object to the headers

--- src/main/java/nl/knmi/adaguc/tools/CGIRunner.java
+++ src/main/java/nl/knmi/adaguc/tools/CGIRunner.java
@@ -6,124 +6,144 @@ import java.util.Calendar;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.JSONObject;
+
 public class CGIRunner {
 
 	/**
-	 * Runs a CGI program as executable on the system. Emulates the behavior of scripts in a traditional cgi-bin directory of apache http server.
-	 * @param commands A string array with the executable and the arguments to this executable
+	 * Runs a CGI program as executable on the system. Emulates the behavior of
+	 * scripts in a traditional cgi-bin directory of apache http server.
+	 * 
+	 * @param commands             A string array with the executable and the
+	 *                             arguments to this executable
 	 * @param environmentVariables The environment variables used for this run
-	 * @param directory The location to start the program, can be null or "" if not needed
-	 * @param response Can be null, when given the content-type for the response will be set.
-	 * @param outputStream A standard byte output stream in which the data of stdout is captured. Can for example be response.getOutputStream().
-	 * @param postData The data to post.
-	 * @return 
-	 * @throws IOException 
-	 * @throws InterruptedException 
+	 * @param directory            The location to start the program, can be null or
+	 *                             "" if not needed
+	 * @param response             Can be null, when given the content-type for the
+	 *                             response will be set.
+	 * @param outputStream         A standard byte output stream in which the data
+	 *                             of stdout is captured. Can for example be
+	 *                             response.getOutputStream().
+	 * @param postData             The data to post.
+	 * @return
+	 * @throws IOException
+	 * @throws InterruptedException
 	 * @throws Exception
 	 */
-	public static int runCGIProgram(String[] commands,String[] environmentVariables,String directory,final HttpServletResponse response,OutputStream outputStream,String postData, long timeOutMs) throws InterruptedException, IOException {
-		Debug.println("Working Directory: "+directory);
+	public static int runCGIProgram(String[] commands, String[] environmentVariables, String directory,
+			final HttpServletResponse response, OutputStream outputStream, String postData, long timeOutMs)
+			throws InterruptedException, IOException {
+		Debug.println("Working Directory: " + directory);
 
-		class StderrPrinter implements ProcessRunner.StatusPrinterInterface{
+		class StderrPrinter implements ProcessRunner.StatusPrinterInterface {
 			StringBuffer errorMessages = new StringBuffer();
-			public void print(byte[] message,int bytesRead) {
-				errorMessages.append(new String(message,0,bytesRead));
+
+			public void print(byte[] message, int bytesRead) {
+				errorMessages.append(new String(message, 0, bytesRead));
 			}
+
 			public void setError(String message) {
 			}
-			public String getError() { 
-				return errorMessages.toString();   
+
+			public String getError() {
+				return errorMessages.toString();
 			}
+
 			@Override
 			public boolean hasData() {
 				return false;
 			}
 		}
-		class StdoutPrinter implements ProcessRunner.StatusPrinterInterface{
+		class StdoutPrinter implements ProcessRunner.StatusPrinterInterface {
 			boolean headersSent = false;
 			boolean foundLF = false;
 			boolean hasError = false;
 			OutputStream output = null;
 			StringBuffer header = new StringBuffer();
+			String pid = "";
+
 			public StdoutPrinter(OutputStream outputStream) {
-				output=outputStream;
+				output = outputStream;
 			}
 
 			// boolean a = false;
-			public void print(byte[] message,int bytesRead) {
+			public void print(byte[] message, int bytesRead) {
 				try {
-					_print(message,bytesRead);
+					_print(message, bytesRead);
 				} catch (IOException e) {
 					this.hasError = true;
-					//Debug.errprintln("Unable to write data: "+e.getMessage());
+					// Debug.errprintln("Unable to write data: "+e.getMessage());
 				}
 			}
 
-			public void _print(byte[] message,int bytesRead) throws IOException {
-				//Try to extract HTML headers and Content-Type 
-				if(hasError)return;
-				if(headersSent==false){
-					int endHeaderIndex=0;
-					for(int j=0;j<bytesRead;j++){
+			public void _print(byte[] message, int bytesRead) throws IOException {
+				// Try to extract HTML headers and Content-Type
+				if (hasError)
+					return;
+				if (headersSent == false) {
+					int endHeaderIndex = 0;
+					for (int j = 0; j < bytesRead; j++) {
 
-						if(message[j] == 10){
-							if(foundLF == false){
+						if (message[j] == 10) {
+							if (foundLF == false) {
 								foundLF = true;
 								continue;
 							}
-						}else if (foundLF == true && message[j] != 13){
-							foundLF=false;
+						} else if (foundLF == true && message[j] != 13) {
+							foundLF = false;
 							continue;
 						}
 
-						if(foundLF == true){
-							if(message[j] == 10){
+						if (foundLF == true) {
+							if (message[j] == 10) {
 								headersSent = true;
 								endHeaderIndex = j;
-								while((message[endHeaderIndex] == 10 || message[endHeaderIndex] == 13 ) && endHeaderIndex<bytesRead){
+								while ((message[endHeaderIndex] == 10 || message[endHeaderIndex] == 13) && endHeaderIndex < bytesRead) {
 									endHeaderIndex++;
 								}
-								output.write(message, endHeaderIndex,bytesRead-(endHeaderIndex));
-								header.append(new String(message),0,endHeaderIndex);
+								output.write(message, endHeaderIndex, bytesRead - (endHeaderIndex));
+								header.append(new String(message), 0, endHeaderIndex);
 								break;
 							}
 						}
 					}
-					if( headersSent == true){
-						String[] headerItem=header.toString().split("\n");
-						for(int h=0;h<headerItem.length;h++){
-							String[] headerKVP=headerItem[h].split(":");
-							if(headerKVP.length==2){
-								//Debug.println(headerKVP[0].trim()+"="+headerKVP[1].trim());
-								if(headerKVP[0].trim().equalsIgnoreCase("Content-Type")){
-									if(response!=null){
-										headerKVP[1]=headerKVP[1].replaceAll("\\n", "");
-										headerKVP[1]=headerKVP[1].replaceAll("\\r", "");
-										headerKVP[1]=headerKVP[1].replaceAll(" ", "");
-										//Debug.println("Setting Content-Type to ["+headerKVP[1].trim()+"]");
+					if (headersSent == true) {
+						response.setHeader("ADAGUCSERVICESINFO", (new JSONObject().put("pid", pid).toString()));
+						String[] headerItem = header.toString().split("\n");
+						for (int h = 0; h < headerItem.length; h++) {
+							String[] headerKVP = headerItem[h].split(":");
+							if (headerKVP.length == 2) {
+								// Debug.println(headerKVP[0].trim()+"="+headerKVP[1].trim());
+								if (headerKVP[0].trim().equalsIgnoreCase("Content-Type")) {
+									if (response != null) {
+										headerKVP[1] = headerKVP[1].replaceAll("\\n", "");
+										headerKVP[1] = headerKVP[1].replaceAll("\\r", "");
+										headerKVP[1] = headerKVP[1].replaceAll(" ", "");
+										// Debug.println("Setting Content-Type to ["+headerKVP[1].trim()+"]");
 										response.setContentType(headerKVP[1].trim());
 									}
-								}else{
-									//Debug.println("Setting header ["+headerKVP[0].trim()+"]=["+headerKVP[1].trim()+"]");
-									response.setHeader(headerKVP[0].trim(),headerKVP[1].trim());
+								} else {
+									// Debug.println("Setting header
+									// ["+headerKVP[0].trim()+"]=["+headerKVP[1].trim()+"]");
+									response.setHeader(headerKVP[0].trim(), headerKVP[1].trim());
 								}
 							}
 						}
-					}else{
+					} else {
 						header.append(message);
 					}
-				}else{
-					output.write(message,0,bytesRead);
+				} else {
+					output.write(message, 0, bytesRead);
 				}
 			}
-
 
 			public void setError(String message) {
 				hasError = true;
 			}
 
 			public String getError() {
-				if(hasError)return "yes";
+				if (hasError)
+					return "yes";
 				return null;
 			}
 
@@ -131,60 +151,65 @@ public class CGIRunner {
 			public boolean hasData() {
 				return headersSent;
 			}
+
+			@Override
+			public void setPID(String pid) {
+				this.pid = pid;
+			}
+
 		}
 
 		ProcessRunner.StatusPrinterInterface stdoutPrinter = new StdoutPrinter(outputStream);
 		ProcessRunner.StatusPrinterInterface stderrPrinter = new StderrPrinter();
 		//
-		//    Debug.println("Environment:");
-		//    for(int j=0;j<environmentVariables.length;j++){
-		//      Debug.println(environmentVariables[j]);
-		//    }
-		//    Debug.println("Commands:");
-		//    for(int j=0;j<commands.length;j++){
-		//      Debug.println(commands[j]);
-		//    }
-		
-		ProcessRunner processRunner = new ProcessRunner (stdoutPrinter,stderrPrinter,environmentVariables,directory, timeOutMs);
+		// Debug.println("Environment:");
+		// for(int j=0;j<environmentVariables.length;j++){
+		// Debug.println(environmentVariables[j]);
+		// }
+		// Debug.println("Commands:");
+		// for(int j=0;j<commands.length;j++){
+		// Debug.println(commands[j]);
+		// }
 
+		ProcessRunner processRunner = new ProcessRunner(stdoutPrinter, stderrPrinter, environmentVariables, directory,
+				timeOutMs);
 
-
-		long startTimeInMillis = Calendar.getInstance().getTimeInMillis();  
+		long startTimeInMillis = Calendar.getInstance().getTimeInMillis();
 		Debug.println("Starting CGI.");
 
-		processRunner.runProcess(commands,postData);
-
+		processRunner.runProcess(commands, postData);
 		long stopTimeInMillis = Calendar.getInstance().getTimeInMillis();
-		if(processRunner.exitValue()!=0){
-			Debug.errprintln("Warning: exit code: "+processRunner.exitValue());
+		if (processRunner.exitValue() != 0) {
+			Debug.errprintln("Warning: exit code: " + processRunner.exitValue());
 		}
-		if(stdoutPrinter.getError()!=null){
+		if (stdoutPrinter.getError() != null) {
 			Debug.errprintln("Warning: errors occured while writing to pipe");
 		}
-		if(response!=null){
-			if(processRunner.exitValue()!=0&&processRunner.exitValue()!=1){
+		if (response != null) {
+			if (processRunner.exitValue() != 0 && processRunner.exitValue() != 1) {
 				response.setStatus(500);
-				String msg="Internal server error: CGI returned code " + processRunner.exitValue();
+				String msg = "Internal server error: CGI returned code " + processRunner.exitValue();
 				if (processRunner.exitValue() == 143) {
-					msg += " (Timeout set to " + timeOutMs + " ms, process took "+((stopTimeInMillis-startTimeInMillis))+" ms)";
+					msg += " (Timeout set to " + timeOutMs + " ms, process took " + ((stopTimeInMillis - startTimeInMillis))
+							+ " ms)";
 				}
 				Debug.errprintln(msg);
 				outputStream.write(msg.getBytes());
 			}
-			if(processRunner.exitValue()!=0){
+			if (processRunner.exitValue() != 0) {
 				String errors = stderrPrinter.getError();
-				Debug.errprintln("Errors during CGI execution: "+errors);
-				if(errors!=null && stdoutPrinter.hasData()==false){
-					if(errors.indexOf("401")!=-1){
+				Debug.errprintln("Errors during CGI execution: " + errors);
+				if (errors != null && stdoutPrinter.hasData() == false) {
+					if (errors.indexOf("401") != -1) {
 						response.setStatus(401);
 						outputStream.write(errors.getBytes());
-					}else  if(errors.indexOf("403")!=-1){
+					} else if (errors.indexOf("403") != -1) {
 						response.setStatus(403);
 						outputStream.write(errors.getBytes());
-					}else if(errors.indexOf("404")!=-1){
+					} else if (errors.indexOf("404") != -1) {
 						response.setStatus(404);
 						outputStream.write(errors.getBytes());
-					}else if(errors.indexOf("Error opening")!=-1){
+					} else if (errors.indexOf("Error opening") != -1) {
 						response.setStatus(415);
 						outputStream.write(errors.getBytes());
 					}
@@ -192,11 +217,11 @@ public class CGIRunner {
 			}
 		}
 
-		
-		Debug.println("Finished CGI with code "+processRunner.exitValue()+": "+" ("+(stopTimeInMillis-startTimeInMillis)+" ms)");
-		try{
+		Debug.println("Finished CGI with code " + processRunner.exitValue() + ": " + " ("
+				+ (stopTimeInMillis - startTimeInMillis) + " ms)");
+		try {
 			outputStream.flush();
-		}catch(org.apache.catalina.connector.ClientAbortException e){
+		} catch (org.apache.catalina.connector.ClientAbortException e) {
 			Debug.println("Stream already closed");
 
 		}
